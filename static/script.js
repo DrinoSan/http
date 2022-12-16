580de18f4abbfd356ad0588b8db21809f80ffcbd
DrinoSan <s.becirbegovic@gmail.com>
2022-12-17 00:46:59 +0100

    Changes by hassan Modified index.html script.js and style.css <-- by Mujtaba @Hassan-Erfani

--- static/script.js
+++ static/script.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => setInterval(() => {
-    console.log(Date.now().toLocaleString());
+    let time = document.getElementById('time');
+    time.innerHTML = Date.now().toLocaleString();
 }, 1000));
+
+function handleSubmit(event) {
+
+    const data = new FormData(event.target);
+    const value = Object.fromEntries(data.entries());
+
+    return value;
+}
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
