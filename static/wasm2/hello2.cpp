f1666e7bed1ba55181fccfc0f1027392815309bc
DrinoSan <s.becirbegovic@gmail.com>
2023-01-05 23:55:21 +0100

    WebAssembly example another one

---
+++ static/wasm2/hello2.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Sandrino Becirbegovic on 05/01/2023.
+//
+
+#include <stdio.h>
+
+int main() {
+	printf("Hello World\n");
+	return 0;
+}
---
+++ static/wasm3/hello3.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Sandrino Becirbegovic on 05/01/2023.
+//
+#include <stdio.h>
+#include <emscripten/emscripten.h>
+
+int main() {
+	printf("Hello World\n");
+	return 0;
+}
+
+#ifdef __cplusplus
+#define EXTERN extern "C"
+#else
+#define EXTERN
+#endif
+
+EXTERN EMSCRIPTEN_KEEPALIVE void myFunction(int argc, char ** argv) {
+	printf("MyFunction Called\n");
+}
